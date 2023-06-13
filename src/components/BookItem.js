e76f8e68252cb2495806ede8aacc689970c3c107
ybadr99 <yousefbadr909@gmail.com>
2023-06-13 18:55:27 +0300

    Dispatch books in the resuable component & dispatch add and remove functionalitiy;

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -1,15 +1,24 @@
+import { useDispatch } from 'react-redux';
 import React from 'react';
+import { removeBook } from '../redux/books/booksSlice';
 
-const BookItem = ({ book, deleteBook }) => (
-  <div>
-    <li className="book-item">
-      <p>{book.name}</p>
-      <span className="author">{book.author}</span>
-      <button onClick={() => deleteBook(book.id)} type="button">
-        Delete
-      </button>
-    </li>
-  </div>
-);
+const BookItem = ({ book }) => {
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <li className="book-item">
+        <p>{book.title}</p>
+        <span className="author">{book.author}</span>
+        <span className="category">{book.category}</span>
+        <button
+          type="button"
+          onClick={() => dispatch(removeBook({ id: book.id }))}
+        >
+          Delete
+        </button>
+      </li>
+    </div>
+  );
+};
 
 export default BookItem;
--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -2,7 +2,26 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  books: [],
+  books: [
+    {
+      id: 'item1',
+      title: 'The Great Gatsby',
+      author: 'John Smith',
+      category: 'Fiction',
+    },
+    {
+      id: 'item2',
+      title: 'Anna Karenina',
+      author: 'Leo Tolstoy',
+      category: 'Fiction',
+    },
+    {
+      id: 'item3',
+      title: 'The Selfish Gene',
+      author: 'Richard Dawkins',
+      category: 'Nonfiction',
+    },
+  ],
 };
 
 const booksSlice = createSlice({
@@ -10,7 +29,7 @@ const booksSlice = createSlice({
   initialState,
   reducers: {
     addBook: (state, { payload }) => {
-      state.books = [...state.books, payload.book];
+      state.books = [...state.books, payload];
     },
     removeBook: (state, { payload }) => {
       state.books = state.books.filter((book) => book.id !== payload.id);
